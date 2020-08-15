559416874bd2b74ec5bdee66b9ddb301916783e5
wonno 노종원 <jake89577@gmail.com>
2020-08-15 12:47:49 +0900

    Update iamport.rb

--- config/initializers/iamport.rb
+++ config/initializers/iamport.rb
@@ -1,4 +1,4 @@
 Iamport.configure do |config|
-  config.api_key = "9404870238699812"
-  config.api_secret = "mInuSPlKWpTvEV9jPyrktOFVFj56Y7flPCQIscsoCmTgh3A6B1q5Q60wq6CoeSK3vALXMemoAgFSkfZV"
+  config.api_key = ENV[:API_KEY]
+  config.api_secret = ENV[:SECRET_KEY]
 end
